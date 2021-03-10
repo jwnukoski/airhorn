cd98f1d4308e5357b32d1eb269e12fcde6999dbb
jwnukoski <jwnukoski@protonmail.com>
2021-03-10 18:41:32 -0500

    init

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
+  const [sound, setSound] = useState();
+  
+  async function play() {
+    const { sound } = await Audio.Sound.createAsync(
+       require('./assets/airhorn.mp3')
+    );
+    setSound(sound)
+
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.fontContainer}>
+        <Text style={styles.font}>Airhorn</Text>
+      </View>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.btnContainer} onPress={play} accessibilityLabel="Play airhorn">
+            <Text style={styles.btnTxt}>Play</Text>
+        </TouchableOpacity>
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  font: {
+    color: 'white',
+    fontSize: 40,
+    marginBottom: 40
+  },
+  btnContainer: {
+    width: '100%',
+    height: 100,
+    marginTop: 10, 
+    backgroundColor: 'white'
+  },
+  btnTxt: {
+    fontSize: 40,
+    paddingTop: 15,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  playBtn: {
+    backgroundColor: 'grey',
+    padding: 10
+  }
 });
